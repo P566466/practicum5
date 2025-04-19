09d4805d5d1a0987b1c69a884ebe2d730c850c8b
Srushti Sonavane <srushclg@gmail.com>
2025-04-18 20:16:23 -0400

    Initial Commit

---
+++ render/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    render = {
+      source  = "render-oss/render"
+      version = "1.5.0"
+    }
+  }
+}
+
+provider "render" {
+  api_key  = "rnd_iUrZU0hbFmEtrvnzHIzyl9kwMn0z"
+  owner_id = "tea-cuu0vqd2ng1s73devmjg"
+}
